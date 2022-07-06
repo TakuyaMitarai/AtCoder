48917d4333205b8a88fff2065947573e08131335
mitarai <aaa>
2022-07-07 01:26:15 +0900

    003

--- 競プロ典型90問/003.cpp
+++ 競プロ典型90問/003.cpp
@@ -3,11 +3,11 @@
 #include <queue>
 using namespace std;
 
-long long N;
-long long A[1 >> 20];
-long long B[1 >> 20];
-vector<int> P[1 >> 20];
-int cnt[1 >> 20];
+int N;
+int A[1 << 20];
+int B[1 << 20];
+vector<int> P[1 << 20];
+int cnt[1 << 20];
 
 void minmax(int s)
 {   
@@ -15,15 +15,15 @@ void minmax(int s)
     int pos;
     queue<int> Q;
     Q.push(s);
-    cnt[pos] = 0;
+    cnt[s] = 0;
 
     while(!Q.empty()) {
         pos = Q.front();
         Q.pop();
-        for(int i : P[pos]) {
-            if(cnt[i] == -1) {
-                cnt[i] = cnt[pos] + 1;
-                Q.push(i);
+        for(int j : P[pos]) {
+            if(cnt[j] == -1) {
+                cnt[j] = cnt[pos] + 1;
+                Q.push(j);
             }
         }
     }
@@ -35,7 +35,7 @@ int main()
     int max_num;
     //入力
     cin >> N;
-    for(int i = 0; i < N - 1; i++) {
+    for(int i = 1; i <= N - 1; i++) {
         cin >> A[i] >> B[i];
         P[A[i]].push_back(B[i]);
         P[B[i]].push_back(A[i]);
@@ -48,7 +48,6 @@ int main()
             max_num = i;
         }
     }
-    cout << max_num << endl;
     max = 0;
 
     minmax(max_num);
