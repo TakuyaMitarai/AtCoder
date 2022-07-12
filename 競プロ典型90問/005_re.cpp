e661fa5eeae2281ca29d30672d69e1223165afda
TakuyaMitarai <miterphoto@gmail.com>
2022-07-12 10:45:54 +0900

    005解き直し

--- 競プロ典型90問/005_re.cpp
+++ 競プロ典型90問/005_re.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 long long N, B, K;
-int c[11]; 
-long long dp[1 << 14][1 << 11];
+int c[10];
+long long dp[10001][31];
 
-int main() 
+int main()
 {
     //入力
     cin >> N >> B >> K;
@@ -16,10 +16,10 @@ int main()
     //動的計画法
     dp[0][0] = 1;
     for(int i = 1; i <= N; i++) {
-        for(int k = 0; k < B; k++) {
-            for(int j = 0; j < K; j++) {
-                int nex = (10 * k + c[j]) % B;
-                dp[i][nex] += dp[i - 1][k];
+        for(int j = 0; j < B; j++) {
+            for(int k = 0; k < K; k++) {
+                int nex = (10 * j + c[k]) % B;
+                dp[i][nex] += dp[i - 1][j];
                 dp[i][nex] %= 1000000007;
             }
         }
@@ -27,5 +27,4 @@ int main()
 
     //出力
     cout << dp[N][0] << endl;
-    return 0;
 }
