ad619975af9b7e9a2cecd95efcdd31ed3ca22830
TakuyaMitarai <miterphoto@gmail.com>
2022-07-15 23:24:04 +0900

    何か勘違いしてそう

--- 競プロ典型90問/006.cpp
+++ 競プロ典型90問/006.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 long long N, K;
 string S;
+vector<int> V;
 
 void sort(int m, int M)
 {
@@ -22,6 +24,9 @@ void sort(int m, int M)
                 int a = S[min];
                 S[min] = S[max];
                 S[max] = a;
+                int b = V[min];
+                V[min] = V[max];
+                V[max] = b;
                 min++;
                 max--;
             }
@@ -31,10 +36,44 @@ void sort(int m, int M)
     }
 }
 
+void k_sort(int m, int M)
+{
+    if(m < M) {
+        long long min = m, max = M;
+        long long mid = (min + max) / 2;
+        long long d = V[mid];
+        do {
+            while(V[min] < d) {
+                min++;
+            }
+            while(V[max] > d) {
+                max--;
+            }
+            if(min <= max){
+                int a = S[min];
+                S[min] = S[max];
+                S[max] = a;
+                int b = V[min];
+                V[min] = V[max];
+                V[max] = b;
+                min++;
+                max--;
+            }
+        } while(min <= max);
+        k_sort(m, max);
+        k_sort(min, M);
+    }
+}
+
 int main()
 {
     cin >> N >> K >> S;
+    for(int i = 0; i < N; i++) {
+        V.push_back(i);
+    }
+
     sort(0, N-1);
+    k_sort(0,K-1);
 
     for(int i = 0; i < K; i++) {
         cout << S[i];
