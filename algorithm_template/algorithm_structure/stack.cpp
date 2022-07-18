b4dc3783db47e0794fc0d5c793e3f4f657257abd
TakuyaMitarai <miterphoto@gmail.com>
2022-07-18 16:57:42 +0900

    スタック

---
+++ algorithm_template/algorithm_structure/stack.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+stack <double> S;
+
+int main()
+{
+    string a;
+
+    //入力
+    cin >> a;
+
+    for(int i = 0; i < a.length(); i++) {
+        if(a[i] == '+') {
+            double x = S.top();
+            S.pop();
+            double y = S.top();
+            S.pop();
+            S.push(x + y);
+        } else if(a[i] == '-') {
+            double x = S.top();
+            S.pop();
+            double y = S.top();
+            S.pop();
+            S.push((y - x));
+        } else if(a[i] == '*') {
+            double x = S.top();
+            S.pop();
+            double y = S.top();
+            S.pop();
+            S.push(x * y);
+        } else if(a[i] == '/') {
+            double x = S.top();
+            S.pop();
+            double y = S.top();
+            S.pop();
+            S.push(y / x);
+        } else {
+            S.push(a[i] - 48);
+        }
+    }
+
+    //出力
+    cout << S.top() << endl;
+    return 0;
+}
